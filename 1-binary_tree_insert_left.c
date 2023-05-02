45522dd179a380dec04b6e25f23ce12ce291774d
psych0wh1z <ikeagudike@gmail.com>
2023-05-02 22:24:59 +0100

    Handle malloc

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,12 +7,13 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	if (parent == NULL)
-		return (NULL);
-
-	binary_tree_t *node = binary_tree_node(parent, value);
+	binary_tree_t *node = malloc(sizeof(binary_tree_t));
 	if (node == NULL)
 		return (NULL);
+
+	if (parent == NULL)
+		return (NULL);
+	node = binary_tree_node(parent, value);
 
 	if (parent->left == NULL)
 	{
