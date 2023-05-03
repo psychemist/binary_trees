29138938bed313322b01bae9af988fabbaefe073
psych0wh1z <ikeagudike@gmail.com>
2023-05-03 11:52:08 +0100

    Fix betty error

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,7 +9,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	return (binary_tree_height(tree->left) - 
+	return (binary_tree_height(tree->left) -
 			binary_tree_height(tree->right));
 }
 
