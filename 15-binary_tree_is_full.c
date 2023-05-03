e3b0fdef20d0eed636438758607e934177f82d8f
psych0wh1z <ikeagudike@gmail.com>
2023-05-03 12:33:21 +0100

    Use logical AND to ensure correct output

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -12,7 +12,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (1);
 
 	if (tree->left && tree->right)
-		return (binary_tree_is_full(tree->left),
+		return (binary_tree_is_full(tree->left) &&
 			binary_tree_is_full(tree->right));
 
 	return (0);
