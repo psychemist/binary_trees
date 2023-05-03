d86e31651c9be9e6dcaa24b733e5ee4ad1526b9e
psych0wh1z <ikeagudike@gmail.com>
2023-05-03 11:34:21 +0100

    Fix typo

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -18,7 +18,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 }
 
 /**
- * max - finds the maximum number od two integers
+ * max - finds the maximum number of two integers
  * @x: first integer
  * @y: second integer
  * Return: max number
