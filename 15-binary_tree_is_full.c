49a70a2bcd65d63548a3b2f41422f20c68ce748d
psych0wh1z <ikeagudike@gmail.com>
2023-05-03 12:29:51 +0100

    Change from post- to pre-order traversal

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -8,12 +8,12 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
+
 	if (tree->left && tree->right)
 		return (binary_tree_is_full(tree->left),
 			binary_tree_is_full(tree->right));
-
-	if (!tree->left && !tree->right)
-		return (1);
 
 	return (0);
 }
