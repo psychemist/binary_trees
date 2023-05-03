9b6c09f6b918b1d3e9544de0249cb1a76b209abf
psych0wh1z <ikeagudike@gmail.com>
2023-05-03 08:46:05 +0100

    Remove helper function

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,16 +1,5 @@
 #include "binary_trees.h"
 /**
- * max - finds the maximum number od two integers
- * @x: first integer
- * @y: second integer
- * Return: max number
- */
-size_t max(size_t x, size_t y)
-{
-	return (x > y ? x : y);
-}
-
-/**
  * binary_tree_depth - measures the depth of a node in a binary tree
  * @tree: pointer to the root node to measure the depth
  * Return: depth of tree or 0 (NULL tree)
