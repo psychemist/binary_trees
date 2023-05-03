9791d4123ae391c7356ff2b4499ea5d57fe7d53a
psych0wh1z <ikeagudike@gmail.com>
2023-05-03 11:41:06 +0100

    Refactor code

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,10 +9,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	int left_tree = binary_tree_height(tree->left);
-	int right_tree = binary_tree_height(tree->right);
-
-	return (left_tree - right_tree);
+	return (binary_tree_height(tree->left) - 
+			binary_tree_height(tree->right));
 }
 
 /**
