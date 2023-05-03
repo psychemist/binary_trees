88f91e8701237f28a4d1ce7f1d0327e49a9838e2
psych0wh1z <ikeagudike@gmail.com>
2023-05-03 08:08:58 +0100

    Add task9 files

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+/**
+ * max - finds the maximum number od two integers
+ * @x: first integer
+ * @y: second integer
+ * Return: max number
+ */
+size_t max(size_t x, size_t y)
+{
+	return (x > y ? x : y);
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
+ * Return: height of tree or 0 (NULL tree)
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	else if (tree && tree->right == NULL && tree->left == NULL)
+		return (0);
+
+	return (max(binary_tree_height(tree->left),
+			binary_tree_height(tree->right)) + 1);
+}
