1c25d41c896890642ce746edced7ffebc86299b1
psych0wh1z <ikeagudike@gmail.com>
2023-05-03 12:22:02 +0100

    Modify recursive path

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,12 +9,10 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	if (tree->left && tree->right)
-		return (binary_tree_is_full(tree->left), 
+		return (binary_tree_is_full(tree->left),
 			binary_tree_is_full(tree->right));
 
 	if (!tree->left && !tree->right)
-		return (1);
-	if (tree->left && tree->right)
 		return (1);
 
 	return (0);
