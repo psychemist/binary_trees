ac2f253cd008d174b0ece6596948f4bfad48279c
psych0wh1z <ikeagudike@gmail.com>
2023-05-05 08:16:44 +0100

    Test new code

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -2,51 +2,80 @@
 
 /**
  * binary_tree_is_perfect - checks if a binary tree is perfect
- * @tree: pointer to the root node of the tree to check
- * Return: 1 (perfect) or 0 (non-perfect tree)
+ * @tree: a pointer to the root node of the tree to check
+ *
+ * Return: 1 if the tree is perfect
+ *         0 if the tree is not perfect
+ *         0 if tree is NULL
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	if (!tree)
-		return (0);
+        size_t height = 0;
+        size_t nodes = 0;
+        size_t power = 0;
 
-	if (tree && !tree->left && !tree->right)
-		return (1);
+        if (!tree)
+                return (0);
 
-	int left_height = _get_left_subtree_height(tree);
-	int right_height = _get_right_subtree_height(tree);
+        if (!tree->right && !tree->left)
+                return (1);
 
-	if (left_height == right_height)
-	{
-		return (binary_tree_is_perfect(tree->left) &&
-			binary_tree_is_perfect(tree->right));
-	}
+        height = binary_tree_height(tree);
+        nodes = binary_tree_size(tree);
 
-	return (0);
+        power = (size_t)_pow_recursion(2, height + 1);
+        return (power - 1 == nodes);
 }
 
 /**
- * get_left_subtree_height - finds the height of left binary subtree
- * @tree: pointer to the root node of the tree to check
- * Return: height of tree or 0 (NULL tree)
+ *_pow_recursion - returns the value of x raised to the power of y
+ *@x: the value to exponentiate
+ *@y: the power to raise x to
+ *Return: x to the power of y, or -1 if y is negative
  */
-int _get_left_subtree_height(const binary_tree_t *tree)
-{
-	if (!tree)
-		return (0);
 
-	return (_get_left_subtree_height(tree->left) + 1);
+int _pow_recursion(int x, int y)
+{
+        if (y < 0)
+                return (-1);
+        if (y == 0)
+                return (1);
+        else
+                return (x * _pow_recursion(x, y - 1));
+
 }
 
 /**
- * get_right_subtree_height - finds the height of left binary subtree
- * @tree: pointer to the root node of the tree to check
- * Return: height of tree or 0 (NULL tree)
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: tree to measure the size of
+ *
+ * Return: size of the tree
+ *         0 if tree is NULL
  */
-int _get_right_subtree_height(const binary_tree_t *tree)
+size_t binary_tree_size(const binary_tree_t *tree)
 {
-	if (!tree)
-		return (0);
+        if (!tree)
+                return (0);
 
-	return (_get_right_subtree_height(tree->right) + 1);
+        return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: tree to measure the height of
+ *
+ * Return: height of the tree
+ *         0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+        size_t height_l = 0;
+        size_t height_r = 0;
+
+        if (!tree)
+                return (0);
+
+        height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+        height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+        return (height_l > height_r ? height_l : height_r);
 }
