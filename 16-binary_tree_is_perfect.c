d31f6cef203e54f2ccc73392be649bbfcdfa55a8
psych0wh1z <ikeagudike@gmail.com>
2023-05-05 07:56:39 +0100

    Modify task16 files

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -6,22 +6,22 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-        if (!tree)
-                return (0);
+	if (!tree)
+		return (0);
 
-        if (tree && !tree->left && !tree->right)
-                return (1);
+	if (tree && !tree->left && !tree->right)
+		return (1);
 
-        int left_height = get_left_subtree_height(tree);
-        int right_height = get_right_subtree_height(tree);
+	int left_height = _get_left_subtree_height(tree);
+	int right_height = _get_right_subtree_height(tree);
 
-        if (left_height == right_height)
-        {
-                return (binary_tree_is_perfect(tree->left) &&
-                        binary_tree_is_perfect(tree->right));
-        }
+	if (left_height == right_height)
+	{
+		return (binary_tree_is_perfect(tree->left) &&
+			binary_tree_is_perfect(tree->right));
+	}
 
-        return (0);
+	return (0);
 }
 
 /**
@@ -29,12 +29,12 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
  * @tree: pointer to the root node of the tree to check
  * Return: height of tree or 0 (NULL tree)
  */
-int get_left_subtree_height(const binary_tree_t *tree)
+int _get_left_subtree_height(const binary_tree_t *tree)
 {
-        if (!tree)
-                return (0);
+	if (!tree)
+		return (0);
 
-        return (get_left_subtree_height(tree->left) + 1);
+	return (_get_left_subtree_height(tree->left) + 1);
 }
 
 /**
@@ -42,10 +42,10 @@ int get_left_subtree_height(const binary_tree_t *tree)
  * @tree: pointer to the root node of the tree to check
  * Return: height of tree or 0 (NULL tree)
  */
-int get_right_subtree_height(const binary_tree_t *tree)
+int _get_right_subtree_height(const binary_tree_t *tree)
 {
-        if (!tree)
-                return (0);
+	if (!tree)
+		return (0);
 
-        return (get_right_subtree_height(tree->right) + 1);
+	return (_get_right_subtree_height(tree->right) + 1);
 }
